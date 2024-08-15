f4d249fbaeab0c6614a13c1aaa2d935761453f8f
Danteon0 <65601627+Danteon0@users.noreply.github.com>
2024-08-15 17:10:07 +0300

    added bot status information

--- events/ready.js
+++ events/ready.js
@@ -12,5 +12,12 @@ module.exports = {
             status: 'online',
         });
 
+        // Discord Activity types
+        //
+        // Listening => Listening to {name}
+        // Playing => Playing {name}
+        // Streaming => Streaming {name}
+        // Watching => Watching {name}
+
     },
 }; // Made by Danteon0 | github.com/Danteon0 <3
