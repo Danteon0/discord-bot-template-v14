f36791ce3202e227f761283d6769d80742deefaf
Danteon0 <65601627+Danteon0@users.noreply.github.com>
2024-08-19 16:36:03 +0300

    remove node_modules & added stats.js
    Added stats.js so you can show your Discord bot stats. Removed the Node_modules folder because packages can be updated and this folder is huge

---
+++ commands/stats.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const os = require('os');
+const config = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('statistics')
+        .setDescription('Displays the bot statistics.'),
+    async execute(interaction) {
+        const uptime = moment.duration(interaction.client.uptime).format("D [days], H [hours], m [minutes], s [seconds]");
+        const statistics = new EmbedBuilder()
+            .setColor(config.embedcolor)
+            .setFooter({ text: 'Saturn Bot Statistics', iconURL: interaction.client.user.avatarURL() })
+            .addFields(
+                { name: "» **Bot Owner**", value: "<@496393095282294796> | Saturn Bot", inline: false },
+                { name: "» **Developer**", value: "<@496393095282294796>", inline: false },
+                { name: "» **Memory Usage**", value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+                { name: "» **Uptime**", value: `${uptime}`, inline: false },
+                { name: "» **Users**", value: `${interaction.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`, inline: true },
+                { name: "» **Servers**", value: `${interaction.client.guilds.cache.size.toLocaleString()}`, inline: true },
+                { name: "» **Channels**", value: `${interaction.client.channels.cache.size.toLocaleString()}`, inline: true },
+                { name: "» **Discord.JS Version**", value: `v${require('discord.js').version}`, inline: true },
+                { name: "» **Node.JS Version**", value: `${process.version}`, inline: true },
+                { name: "» **Ping**", value: `${interaction.client.ws.ping} ms`, inline: true },
+                { name: "**» Bot Invite**", value: "[Invite](YOUR BOT INVITE ADRESS)", inline: false },
+                { name: "**» Support Server**", value: "[Join our server](YOUR SUPPORT SERVER)", inline: false },
+                { name: "**» Vote Page**", value: "[Vote for us!](VOTE LINK)", inline: false }
+            );
+        
+        await interaction.reply({ embeds: [statistics] });
+    },
+};
